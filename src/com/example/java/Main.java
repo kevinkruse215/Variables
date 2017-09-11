83f47d6c3808812c30861e8e0127f490ae6f5308
kkruse13 <kkruse13@students.mchenry.edu>
2017-09-10 20:39:20 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       byte byteExample=1;
+       short shortExample=1234;
+       int intExample=123456789;
+       long longExample= 1234567L ;
+       float floatExample = 12.3f;
+       double doubleExample= 12.3456d;
+       boolean booleanExample= true;
+       char charExample= 1234;
+
+        System.out.println("Byte value = " + byteExample +": a byte is an 8-bit signed integer");
+        System.out.println("short value = " + shortExample+": a short is a 16-bit signed integer");
+        System.out.println("int value = " + intExample+": an int is a 32-bit signed integer");
+        System.out.println("long value = " + longExample+" a long is a 64-bit integer");
+        System.out.println("float value = " + floatExample+": a float is a single precision floating point");
+        System.out.println("double value = " + doubleExample+": a double is a 64-bit floating point");
+        System.out.println("boolean value = " + booleanExample+ ": a boolean is a true or false value");
+        System.out.println("char value = " + charExample+": a char is a single 16-bit Unicode character");
+
+    }
+}
